fd081e8600400fe47e4370c25b98b08ce10e7fa6
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-07-02 20:10:48 +0530

    select and timeout

---
+++ timeouts/timeouts.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c1 <- "result 1"
+	}()
+
+	select {
+	case r1 := <-c1:
+		fmt.Println(r1)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
+	}()
+
+	select {
+	case r2 := <-c2:
+		fmt.Println(r2)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
+	}
+}
